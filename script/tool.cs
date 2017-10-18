c6c979bbed2283f572e190e6ea0c0122a9755491
9MW <9MrsWang9@gmail.com>
2017-10-18 17:57:53 +0800

    editor tool
    for auxiliary work,I will extend this in future.
    this time for convert jpg to png format.It can convert specify color to transparency

---
+++ script/tool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+[CustomEditor(typeof(T))]//T is gameobject component,muet be full else if the extension can't work
+public class tool : Editor {
+    Color Transparency;//The color will be instead by 0,0,0,0,
+    Texture2D outTex;
+    Texture2D inTex;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Transparency Color", GUILayout.Width(130));
+        Transparency = EditorGUILayout.ColorField(Transparency);
+        EditorGUILayout.EndHorizontal();
+        if (EditorGUILayout.DropdownButton(new GUIContent("Generate"), FocusType.Keyboard))
+        {
+            outTex = new Texture2D(inTex.width, inTex.height);
+            Color[] c = inTex.GetPixels();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == Transparency)
+                {
+                    c[i] = new Color(0, 0, 0, 0);
+                }
+            }
+            outTex.SetPixels(c);
+            outTex.Apply();
+            File.WriteAllBytes(Application.dataPath + "/SavedScreen.png",
+            outTex.EncodeToPNG());
+            Debug.Log("already accomplish");
+        }
+    }
+}
